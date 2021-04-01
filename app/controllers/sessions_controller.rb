0cd365c661f9002e8ac7a4eeecb1edb9b0ec8306
vagrant <sknoop@hotmail.ca>
2021-04-01 18:33:18 +0000

    create sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,12 @@
+class SessionsController < ApplicationController
+  #where we create & destroy sessions (aka login and logout)
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+  
+end
