1d76c41b85c4f297f70c682d0be59da8789b09e9
vagrant <sknoop@hotmail.ca>
2021-04-06 15:58:41 +0000

    add user validation tests. Email validation test currently failing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,19 @@
 class User < ActiveRecord::Base
   has_secure_password
+
   validates :email,  presence: true, uniqueness: { case_sensitive: false }
   validates :first_name,  presence: true, length: { minimum: 3 }
   validates :last_name,  presence: true, length: { minimum: 3 }
   validates :password,  presence: true, length: { in: 6..20 }
+  before_save { self.email.downcase!}
+
+  def self.authenticate_with_credentials(email, password)
+    email = email.downcase.strip
+    @user = User.find_by_email(email.downcase.strip)
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,73 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "should save when all fields filled out" do
+    @user = User.create(
+      first_name: "Hello", 
+      last_name: "World", 
+      email: "hello@world.com", 
+      password: "notASafePassword", 
+      password_confirmation: "notASafePassword")
+    expect(@user).to be_valid
+  end
+
+  it "should not save when password and password confirmation do not match" do
+    @user = User.create(
+      first_name: "Hello", 
+      last_name: "World", 
+      email: "hello@world.com", 
+      password: "notASafePassword", 
+      password_confirmation: "SafePassword")
+    expect(@user).to_not be_valid
+  end
+
+  it "should not save when first name is missing" do
+    @user = User.create(
+      first_name: nil, 
+      last_name: "World", 
+      email: "hello@world.com", 
+      password: "notASafePassword", 
+      password_confirmation: "SafePassword")
+    expect(@user).to_not be_valid
+  end
+
+  it "should not save when last name is missing" do
+    @user = User.create(
+      first_name: "Hello", 
+      last_name: nil, 
+      email: "hello@world.com", 
+      password: "notASafePassword", 
+      password_confirmation: "SafePassword")
+    expect(@user).to_not be_valid
+  end
+
+  it "should not save when first name is missing" do
+    @user = User.create(
+      first_name: "Hello", 
+      last_name: "World", 
+      email: nil, 
+      password: "notASafePassword", 
+      password_confirmation: "SafePassword")
+    expect(@user).to_not be_valid
+  end
+
+  it "should not save when email already exists in database" do
+    user1 = User.create(
+      first_name: "Hello", 
+      last_name: "World", 
+      email: "hello@world.com", 
+      password: "notASafePassword", 
+      password_confirmation: "SafePassword"
+    )
+    user2 = User.create(
+      first_name: "Goodbye", 
+      last_name: "Globe", 
+      email: "HELLO@world.com", 
+      password: "GetABetterPassword", 
+      password_confirmation: "GetABetterPassword"
+    )
+    expect(user2).to_not be_valid
+  end
+
 end
