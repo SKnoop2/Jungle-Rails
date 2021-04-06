e7f94a7523c067697b0bfa55e66bc8720dd1da0d
vagrant <sknoop@hotmail.ca>
2021-04-06 20:02:54 +0000

    refactor tests to reduce repetition in code

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,58 +1,43 @@
 require "rails_helper"
 
 RSpec.describe Product, type: :model do
+
+  before :each do
+    @category = Category.new(name: "test_category")
+    @product = Product.new(
+      name: "test_product", 
+      price_cents: 1000, 
+      quantity: 5, 
+      category: @category
+    )
+  end
+
   describe 'Validations' do
 
     it "should save when all fields filled out" do
-      @category = Category.new(name: "test_category")
-      @product = Product.new(
-        name: "test_product", 
-        price_cents: 1000, 
-        quantity: 5, 
-        category: @category )
       expect(@product).to be_valid
     end
 
     it "should not save when name is missing" do
-      @category = Category.new(name: "test_category")
-      @product = Product.new(
-        name: nil, 
-        price_cents: 1000, 
-        quantity: 5, 
-        category: @category )
+      @product.name = nil 
       expect(@product).to_not be_valid
       expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     it "should not save when price is missing" do
-      @category = Category.new(name: "test_category")
-      @product = Product.new(
-        name: "test_product", 
-        price_cents: nil, 
-        quantity: 5, 
-        category: @category )
+      @product.price_cents = nil 
       expect(@product).to_not be_valid
       expect(@product.errors.full_messages).to include("Price can't be blank")
     end
 
     it "should not save when quantity is missing" do
-      @category = Category.new(name: "test_category")
-      @product = Product.new(
-        name: "test_product", 
-        price_cents: 1000, 
-        quantity: nil, 
-        category: @category )
+      @product.quantity = nil
       expect(@product).to_not be_valid
       expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
     
     it "should not save when category is missing" do
-      @category = Category.new(name: "test_category")
-      @product = Product.new(
-        name: "test_product", 
-        price_cents: 1000, 
-        quantity: 5, 
-        category: nil )
+      @product.category = nil
       expect(@product).to_not be_valid
       expect(@product.errors.full_messages).to include("Category can't be blank")
     end
